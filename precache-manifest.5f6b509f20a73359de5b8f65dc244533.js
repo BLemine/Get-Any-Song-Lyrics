e21ba0b07109c223c5cdd9de7c49ce49d23075b6
BLemine <baillahiamine@gmail.com>
2020-06-30 15:46:45 +0100

    Updates

---
+++ precache-manifest.5f6b509f20a73359de5b8f65dc244533.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a300450a296ce54b11d438d8280996b",
+    "url": "/Get-Any-Song-Lyrics/index.html"
+  },
+  {
+    "revision": "768b39f018de55caff94",
+    "url": "/Get-Any-Song-Lyrics/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "101683dc329ff8b24f87",
+    "url": "/Get-Any-Song-Lyrics/static/js/2.c243e5bf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Get-Any-Song-Lyrics/static/js/2.c243e5bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768b39f018de55caff94",
+    "url": "/Get-Any-Song-Lyrics/static/js/main.5a38a831.chunk.js"
+  },
+  {
+    "revision": "7796fa80dfac1a01a278",
+    "url": "/Get-Any-Song-Lyrics/static/js/runtime-main.a686d1bc.js"
+  }
+]);
